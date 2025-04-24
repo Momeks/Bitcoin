961847a18bfe77d0cdfdc6bd0677ebd96cdf2c30
Mohammad Komeili <mkomeili@newstore.com>
2025-04-24 09:37:18 +0200

    Initial Commit

---
+++ Bitcoin/BitcoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitcoinApp.swift
+//  Bitcoin
+//
+//  Created by Mohammad Komeili on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct BitcoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
