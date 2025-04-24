0bd40419d21814f49112b38a57c447d771a33334
Mohammad Komeili <mkomeili@newstore.com>
2025-04-24 13:59:07 +0200

    Add coingecko endpoints

---
+++ Bitcoin/Resource/API/CoinGeckoEndpoint.swift
@@ -0,0 +1,77 @@
+//
+//  CoinGeckoEndpoint.swift
+//  Bitcoin
+//
+//  Created by Mohammad Komeili on 4/24/25.
+//
+
+import Foundation
+import NetworkKit
+
+enum CoinGeckoPath {
+    case coin(id: String)
+    case marketChart(id: String, vsCurrency: String, days: String)
+
+    var id: String {
+        switch self {
+        case .coin(let id), .marketChart(let id, _, _):
+            return id
+        }
+    }
+
+    var path: String {
+        switch self {
+        case .coin(let id):
+            return "coins/\(id)"
+        case .marketChart(let id, _, _):
+            return "coins/\(id)/market_chart"
+        }
+    }
+
+    var queryItems: [URLQueryItem] {
+        switch self {
+        case .coin:
+            return []
+        case .marketChart(_, let vsCurrency, let days):
+            return [
+                URLQueryItem(name: "vs_currency", value: vsCurrency),
+                URLQueryItem(name: "days", value: days)
+            ]
+        }
+    }
+}
+
+struct CoinGeckoEndpoint: Endpoint {
+    let pathType: CoinGeckoPath
+    let apiKey: String
+
+    var id: String {
+        pathType.id
+    }
+
+    var baseURL: URL {
+        URL(string: "https://api.coingecko.com/api/v3")!
+    }
+
+    var path: String {
+        pathType.path
+    }
+
+    var method: HTTPMethod {
+        .get
+    }
+
+    var headers: [String: String]? {
+        nil
+    }
+
+    var queryItems: [URLQueryItem]? {
+        var items = pathType.queryItems
+        items.append(URLQueryItem(name: "x_cg_demo_api_key", value: apiKey))
+        return items
+    }
+
+    var body: Data? {
+        nil
+    }
+}
